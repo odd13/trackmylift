0fec3a12a1ad63d19fc878aa3d81218b503eb635
Oddie <arcadeoddie@gmail.com>
2017-02-11 22:06:29 +1100

    updated calculations for PR

--- app/controllers/exercisegroups_controller.rb
+++ app/controllers/exercisegroups_controller.rb
@@ -1,6 +1,7 @@
 class ExercisegroupsController < ApplicationController
   def index
     @exercisegroups = Exercisegroup.all
+    lift_percentages
   end
 
   def new
@@ -9,5 +10,19 @@ class ExercisegroupsController < ApplicationController
 
   def show
     @exercisegroup = Exercisegroup.find(params[:id])
+    lift_percentages
+  end
+
+  def lift_percentages
+    if @top_exercise = Exerciselog.order(weight: :desc).where('exercisegroup_id = ?', params[:id]).first
+      @lift_percentage_returns = [
+                                [90, (@top_exercise.weight * 0.9)],
+                                [80, (@top_exercise.weight * 0.8)],
+                                [70, (@top_exercise.weight * 0.7)],
+                                [60, (@top_exercise.weight * 0.6)],
+                                [50, (@top_exercise.weight * 0.5)]]
+    end
+
+
   end
 end
